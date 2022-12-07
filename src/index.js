9153f5d5cf786995a9d0de31fb3ff78ff96cd050
Evalyne306 <evalyne.wambui@student.moringaschool.com>
2022-12-07 05:33:30 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+//document.addEventListener("DOMContentLoaded", () => {
+let btn = document.getElementById("submitData");
+
+btn.addEventListener("click", (e)=>{
+e.preventDefault();
+let data = document.getElementById("new-task-description").value;
+buildToDo(data)
+
 });
+
+function buildToDo(todo){
+let p = document.createElement("p");
+let btn = document.createElement("button");
+btn.addEventListener('click', handledelete)
+btn.textContent =  "x";
+p.textContent = `${todo}`
+p.appendChild(btn)
+console.log(p);
+document.querySelector("#list").appendChild(p);
+}
+function handledelete(e){
+  e.target.parentNode.remove()
+}
+
